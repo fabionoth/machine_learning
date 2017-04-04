c1698d6740209200d440ae914140c6099276887b
Fabio Noth <fabio.noth@gmail.com>
2017-04-04 11:57:20 -0300

    inclusao de aula 6

---
+++ aula-6.py
@@ -0,0 +1,5 @@
+#Train an Image Classfier with TensorFlr for Poets
+#TensorFlow
+#image Classification
+
+#https://codelabs.developers.google.com/codelabs/tensorflow-for-poets/#0step 6 - medindo distancia
